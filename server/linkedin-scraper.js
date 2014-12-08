1c9b7fb2f6a74a84f2213b42424d5c0e169ea9b4
Mikael Dalkvist <mikael.dalkvist@gmail.com>
2014-12-08 10:19:23 +0100

    Handle named arguments

--- server/linkedin-scraper.js
+++ server/linkedin-scraper.js
@@ -1,5 +1,28 @@
 var http = require('http');
 
+
+var i = 2;
+var port = 1337;
+var url;
+
+while (process.argv[i]) {
+	switch (process.argv[i]) {
+		case '-p':
+			i++;
+			if (process.argv[i] && Number(process.argv[i])) {
+				port = process.argv[i];
+			}
+			break;
+		case '-u':
+			i++;
+			if (process.argv[i]) {
+				url = process.argv[i];
+			}
+			break;
+	}
+	i++;
+}
+
 var server = http.createServer(function (req, res) {
 	if (req.method != 'GET') {
 		res.writeHead(404);
@@ -9,7 +32,5 @@ var server = http.createServer(function (req, res) {
 	res.writeHead(200, { 'Content-Type': 'application/json' });
 	res.end();
 });
-
-var port = Number(process.argv[2]) || 1337;
 
 server.listen(port);
